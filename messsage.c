537298b968ddd1a2f1710fe10b5f0b3984675939
Emelu David <68957621+davidemelu@users.noreply.github.com>
2022-01-16 18:22:58 +0100

    Add files via upload

---
+++ messsage.c
@@ -0,0 +1,18 @@
+//  Created by David Emelu on 2022-01-16.
+
+/*
+ File: message.c
+ Purpose: Displaying a text message
+ */
+#include <stdio.h>
+
+int main(void) {
+    // first design using slashes
+    printf("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\ \n");
+    printf("Computer Science, Yes!!! \n");
+    printf("======================== \n");
+    printf("And I love coding in C!!!\n");
+    printf("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/ \n");
+    return 0;
+}
+// written by David Emelu Chbuike
