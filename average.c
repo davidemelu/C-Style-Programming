194f658124c4261b69de1faca7126bbfbd485dac
Emelu David <68957621+davidemelu@users.noreply.github.com>
2022-01-16 20:48:03 +0100

    Update average.c

--- average.c
+++ average.c
@@ -5,10 +5,14 @@
 */
 #include <stdio.h>
 int main(){
+    //variable decleration
     int num1, num2, num3;
+    // assigning the numbers to variables
     num1 = 4;
     num2 = 5;
     num3 = 6;
+    // simple formular for average
     float avg = (4 + 5  + 6) / 3;
+    // output
     printf("Average of %d, %d, %d is %.2f \n", num1, num2, num3, avg);
 }
