3681c510c3f453a3440a3a1a5be260c6b7f0bcd6
Emelu David <68957621+davidemelu@users.noreply.github.com>
2022-01-16 19:49:47 +0100

    Add files via upload

---
+++ name.c
@@ -0,0 +1,17 @@
+/*  Created by David Emelu on 2022-01-16.
+* Names.c
+*
+* Prints a list of names with their hometowns
+* and intended major
+*/
+#include <stdio.h>
+int main()
+{
+printf("\n");
+printf("\tName\t\t\tHometown\n");
+printf("\t====\t\t\t========\n");
+printf("\tAlbert Einstein\t\tUlm\n");
+printf("\tDavid Emelu \t Edmonton\n");
+printf("\n");
+return 0;
+}
